be5b616e81b3d5176714e2e6be613bc5921bd862
Ryuya <ryu23896@yahoo.co.jp>
2015-10-01 16:06:52 +0900

    電卓。製作途中

---
+++ calculator.php
@@ -0,0 +1,80 @@
+<html>
+    <head>
+        <meta charset-"UTF-8">
+              <title>テクアカ用電卓</title>
+    </head>
+
+    <body>
+
+        <form method="post"  action="index.php">
+            <input type="text" name="leftNumber" />
+            <select name="operator">
+                <option>+</option>
+                <option>-</option>
+                <option>*</option>
+                <option>/</option>
+            </select>
+            <input type="text" name="rightNumber" />
+            <input type="submit" value="計算する" />
+
+        </form>
+
+    </body>
+
+    <?php
+    $left = $_POST[`leftNumber`];
+    $ope = $_POST[`operator`];
+    $right = $_POST[`rightNumber`];
+    print $left;
+    if ($_SERVER[`REQUEST_METHOD`] == POST) {
+
+        switch ($ope) {
+            case `+`:
+                $answer = $left + $right;
+                break;
+            case `-`:
+                $answer = $left - $right;
+                break;
+            case `*`:
+                $answer = $left * $right;
+                break;
+            case `/`:
+                $answer = $left / $right;
+                break;
+            default :
+                print `???`;
+                break;
+        }
+     
+    }
+print $answer     
+        
+    ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
