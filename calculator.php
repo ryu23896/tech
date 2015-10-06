8742fb0d03d814d469e05116b61c8709b0883f44
Ryuya <ryu23896@yahoo.co.jp>
2015-10-06 17:36:31 +0900

    電卓

--- calculator.php
+++ calculator.php
@@ -1,3 +1,47 @@
+<?php
+error_reporting(-1);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+// 右辺と左辺の値が正しいかチェック
+    if (is_numeric($_POST['leftNumber']) && is_numeric($_POST['rightNumber'])) {
+        $left = $_POST['leftNumber'];
+        $right = $_POST['rightNumber'];
+    } else {
+        print '<center>文字は入力出来ません！</center>';
+    }
+
+// 演算子が正しいかチェック
+    if ($_POST['operator'] =='+' or $_POST['operator'] == '-' or $_POST['operator'] == '*' or $_POST['operator'] == '/') {
+        $ope = $_POST['operator'];    
+    } else {
+        print '<center>演算子が正しくありません</center>';
+    }
+    
+    
+//　演算子に合わせて計算
+    switch ($ope) {
+        case '+':
+            $answer = $left + $right;
+            break;
+        case '-':
+            $answer = $left - $right;
+            break;
+        case '*':
+            $answer = $left * $right;
+            break;
+        case '/':
+            $answer = $left / $right;
+            break;
+        default :
+            print '???';
+            break;
+    }
+
+    print "<center>{$left}  {$ope}  {$right}  は {$answer}  です<center>";
+}
+
+?>
 <html>
     <head>
         <meta charset "UTF-8">
@@ -5,8 +49,8 @@
     </head>
 
     <body>
-
-        <form method="post"  action="index.php">
+ 
+    <center><H1><form method="post"  action="bunri.php">
             <input type="text" name="leftNumber" />
             <select name="operator">
                 <option>+</option>
@@ -17,43 +61,13 @@
             <input type="text" name="rightNumber" />
             <input type="submit" value="計算する" />
 
-        </form>
+            </form><center></H1>
 
+        
     </body>
 
-    <?php
-    $left = $_POST['leftNumber'];
-    $ope = $_POST['operator'];
-    $right = $_POST['rightNumber'];
-    
-    
-    
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        switch ($ope) {
-            case '+':
-                $answer = $left + $right;
-                break;
-            case '-':
-                $answer = $left - $right;
-                break;
-            case '*':
-                $answer = $left * $right;
-                break;
-            case '/':
-                $answer = $left / $right;
-                break;
-            default :
-                print '???';
-                break;
-        }
-     
-    print "{$left}  {$ope}  {$right}  は {$answer}  です"; 
-    }
- 
-        
-    ?>
-</body>
+
 </html>
 
 
